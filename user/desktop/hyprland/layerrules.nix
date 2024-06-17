a02f8c64098d5fa9ef4c508ffeea78235edd74ac
Lorenz Feiner <lorenzfeiner@proton.me>
2024-06-17 16:51:40 +0200

    Added slide animation and blur to rofi
    Blur does not work

--- user/desktop/hyprland/layerrules.nix
+++ user/desktop/hyprland/layerrules.nix
@@ -3,7 +3,12 @@
 {
   wayland.windowManager.hyprland.settings = {
     layerrule = [
+      # wlogout
       "blur, logout_dialog"
+
+      # rofi
+      "animation slide, rofi"
+      "blur, rofi"
     ];
   };
 }
