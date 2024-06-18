47f414d27d9cb0ff9b9c0be8f7d4c6ea1abca4d6
Lorenz Feiner <lorenzfeiner@proton.me>
2024-06-18 14:04:10 +0200

    Moved settings to host folders.
    Previously, all hosts were using the same settings, now they can be
    changed for individual hosts.

---
+++ hosts/personal/systemSettings.nix
@@ -0,0 +1,12 @@
+# ------------------------- #
+# System Settings --------- #
+# ------------------------- #
+
+{
+  system = "x86_64-linux";
+  timezone = "Europe/Vienna";
+  locale = "en_US.UTF-8";
+
+  # Set to true/false depending on if the host is a laptop
+  isLaptop = false;
+}
