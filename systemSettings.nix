4e6c7a1c71800600bb40c60e73f00e941cefcb8a
Lorenz Feiner <lorenzfeiner@proton.me>
2024-06-18 14:12:24 +0200

    Removed now unused files

--- systemSettings.nix
+++
@@ -1,9 +0,0 @@
-# ------------------------- #
-# System Settings --------- #
-# ------------------------- #
-
-{
-  system = "x86_64-linux";
-  timezone = "Europe/Vienna";
-  locale = "en_US.UTF-8";
-}
