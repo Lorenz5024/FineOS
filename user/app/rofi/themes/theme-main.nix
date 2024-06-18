9e929b55c28670966def74ef5dc34f0a57f2a516
Lorenz Feiner <lorenzfeiner@proton.me>
2024-06-18 18:56:24 +0200

    Tweaked theme

--- user/app/rofi/themes/theme-main.nix
+++ user/app/rofi/themes/theme-main.nix
@@ -1,95 +1,86 @@
 { config, ... }:
 
 {
+  home.file.".config/rofi/themes/theme-main.rasi".text = ''
+    * {
+      background:         #${config.lib.stylix.colors.base00}B3;
+      text:               #${config.lib.stylix.colors.base05};
+      entry-background:   #${config.lib.stylix.colors.base02};
 
-  home.file = {
-    ".config/rofi/themes/theme-main.rasi".text = ''
-      * {
-          background: #${config.lib.stylix.colors.base00};
-          base-transparent: #1e1e2e70;
-          text: #cdd6f4;
-          surface0: #313244;
+      prompt-background:  #${config.lib.stylix.colors.base0D};
+      selected-element:   #${config.lib.stylix.colors.base08};
 
-          mauve: #cba6f7;
-          blue: #89b4fa;
-          red: #f38ba8;
+      width: 100%;
+      height: 100%;
+      font: "JetBrainsMono Nerd Font 20";
+      
+      background-color: transparent;
 
-          width: 100%;
-          height: 100%;
-          font: "JetBrainsMono Nerd Font 20";
-          
-          background-color: transparent;
-          text-color: @text;
+      margin: 0px;
+      padding: 0px;
+    }
 
-          margin: 0px;
-          padding: 0px;
-      }
+    window {
+        background-color: @background;
 
-      window {
-          background-color: @background;
+        height: 100%;
+        width: 18em;
 
-          /*
-          border: 2px;
-          border-color: @blue;
-          border-radius: 10px;
-          */
+        location: west;
+        anchor: west;
+    }
 
-          height: 100%;
-          width: 18em;
+    inputbar {
+        children: [prompt,entry];
+        margin: 16px 18px 12px 18px;
+    }
 
-          location: west;
-          anchor: west;
-      }
+    prompt {
+        font: "JetBrainsMono Nerd Font 24";
+        margin: 18px 0px 18px 18px;
+        padding: 6px 16px;
+        border-radius: 10px 0px 0px 10px;
+        vertical-align: 0.5;
+        text-style: bold;
+        background-color: @prompt-background;
+    }
 
-      inputbar {
-          children: [prompt,entry];
-          margin: 16px 18px 12px 18px;
-      }
+    entry {
+        margin: 18px 18px 18px 0px;
+        padding: 6px 16px;
+        border-radius: 0px 10px 10px 0px;
+        vertical-align: 0.5;
+        background-color: @entry-background;
+        text-color: @text;
+    }
 
-      prompt {
-          font: "JetBrainsMono Nerd Font 24";
-          margin: 18px 0px 18px 18px;
-          padding: 6px 16px;
-          border-radius: 10px 0px 0px 10px;
-          vertical-align: 0.5;
-          text-style: bold;
-          background-color: @blue;
-      }
+    listview {
+        margin: 0px 24px 18px 24px;
+        columns: 1;
+        lines: 5;
+    }
 
-      entry {
-          margin: 18px 18px 18px 0px;
-          padding: 6px 16px;
-          border-radius: 0px 10px 10px 0px;
-          vertical-align: 0.5;
-          background-color: @surface0;
-      }
+    element {
+        background-color: transparent;
+        text-color: @text;
+        border-radius: 5px;
+        margin: 1px 0px;
+        padding: 4px 8px;
+    }
 
-      listview {
-          margin: 0px 24px 18px 24px;
-          columns: 1;
-          lines: 5;
-      }
+    element-icon {
+        size: 25px;
+    }
 
-      element {
-          background-color: transparent;
-          border-radius: 5px;
-          margin: 1px 0px;
-          padding: 4px 8px;
-      }
+    element-text {
+      background-color: inherit;
+      text-color: inherit;
+    }
 
-      element-icon {
-          size: 25px;
-      }
+    element selected {
+      text-color: @selected-element;
+      background-color: inherit;
+    }
 
-      element-text {
-       background-color: inherit;
-       text-color: inherit;
-      }
-
-      element selected {
-          text-color: @red;
-      }
-
-    '';
-  };
+  '';
 }
