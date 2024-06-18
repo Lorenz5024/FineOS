8a59d9314ed30e80ef2b8ea409d918cc4c3a6f50
Lorenz Feiner <lorenzfeiner@proton.me>
2024-06-18 18:56:30 +0200

    Tweaked theme

--- user/app/rofi/themes/theme-wallpaperpicker.nix
+++ user/app/rofi/themes/theme-wallpaperpicker.nix
@@ -2,39 +2,45 @@
 
 {
   home.file.".config/rofi/themes/theme-wallpaperpicker.rasi".text = ''
+    *{
+      background:         #${config.lib.stylix.colors.base00}B3;
+      text:               #${config.lib.stylix.colors.base05};
+      element-background:   #${config.lib.stylix.colors.base02};
+
+      prompt-background:  #${config.lib.stylix.colors.base0D};
+      selected-element:   #${config.lib.stylix.colors.base0F};
+
+      background-color: transparent;
+    }
+
     /* ---- Window ---- */
     window {
-        width:					700px;
-            /*height:				450px;*/
-        x-offset:               0px;
-        y-offset:               0px;
-        spacing:                0px;
-        padding:                2px;
-        margin:                 0px; 
-        border:                 2px;
-        border-color:           @active-background;
-        cursor:                 "default";
-        location:               center;
-        anchor:                 center;
-        fullscreen:             false;
-        enabled:                true;
-        border-radius:          12px;
-        border-radius:          12px;
+      background-color: @background;
+      width:		      100%;
+      height:                 25em;
+      x-offset:               0px;
+      y-offset:               0px;
+      spacing:                0px;
+      padding:                2px;
+      margin:                 0px; 
+      cursor:                 "default";
+      location:               south;
+      anchor:                 south;
+      fullscreen:             false;
+      enabled:                true;
     }
 
     /* ---- Mainbox ---- */
     mainbox {
         enabled:                true;
         orientation:            horizontal;
-        padding:		    8px;
+        padding:		8px;
         children:               [ "imagebox"];
         border-radius:          12px;
-        background-color:       transparent;
     }
 
     /* ---- Imagebox ---- */
     imagebox {
-        background-color:       transparent;
         orientation:            vertical;
         children:               [ "inputbar", "listbox"];
     }
@@ -47,12 +53,20 @@
         border-radius:          10px;
         border:                 1px;
         border-color:           @active-background;
-            background-color:		@background;
     }
 
     /* ---- Dummy ---- */
     dummy {
         background-color:       transparent;
+    }
+
+    prompt {
+      font: "JetBrainsMono Nerd Font 18";
+      margin: 0.2em 1em 0.2em 1em;
+      padding: 6px 16px;
+      border-radius: 10px 0px 0px 10px;
+      vertical-align: 0.5;
+      text-style: bold;
     }
 
     /* ---- Inputbar ---- */
@@ -63,135 +77,80 @@
         padding:                8px;
         border-radius:          10px;
         border-color:           @foreground;
-            background-color:		@background;
-        children:               [ "textbox-prompt-colon", "entry" ];
+        children:               [ "entry" ];
         border:                 1px;
         border-color:           @active-background;
     }
 
-    textbox-prompt-colon {
-        enabled:                true;
-        expand:                 false;
-        str:                    "󰸉 ";
-        text-color:             inherit;
-            background-color:		transparent;
-    }
-
     entry {
         enabled:                true;
-        text-color:             inherit;
+        text-color:             @text;
         cursor:                 text;
         placeholder:            "Choose Wallpaper";
         placeholder-color:      inherit;
-            background-color:		transparent;
+        background-color:	transparent;
     }
 
     /* ---- Listview ---- */
     listview {
             enabled:				true;
-            columns:				4;
-            lines:					3;
+            columns:                1;
+            lines:		    10;
             spacing:                4px;
             dynamic:                true;
             cycle:                  true;
             scrollbar:              true;
-            layout:                 vertical;
+            layout:                 horizontal;
             reverse:                false;
             fixed-height:           true;
             fixed-columns:          false;
-            background-color:       transparent;
             border-radius:          10px;
     }
 
     /* ---- Element ---- */
     element {
         enabled:                true;
-        padding:                5px;
+        padding:                0em 2em;
         margin:                 2px;
         cursor:                 pointer;
-            orientation: 			vertical;
+        orientation: 		vertical;
         background-color:       transparent;
         border-radius:          10px;
         border:                 0px;
     }
 
-    element normal.normal {
-        background-color:       inherit;
-        text-color:             @foreground;
-    }
-
-    element normal.urgent {
-        background-color:      inherit;
-        text-color:            @foreground;
-    }
-
-    element normal.active {
-        background-color:      inherit;
-        text-color:            @foreground;
-    }
-
-    element selected.normal {
-        background-color:      @selected-normal-background;
-        text-color:            @foreground;
-    }
-
-    element selected.urgent {
-        background-color:      inherit;
-        text-color:            @foreground;
-    }
-
-    element selected.active {
-        background-color:      inherit;
-        text-color:            @foreground;
-    }
-
-    element alternate.normal {
-        background-color:      inherit;
-        text-color:            @foreground;
-    }
-
-    element alternate.urgent {
-        background-color:      inherit;
-        text-color:            @foreground;
-    }
-
-    element alternate.active {
-        background-color:      inherit;
+    element selected {
+        background-color:      @selected-element;
         text-color:            @foreground;
     }
 
     element-icon {
-        background-color:      transparent;
         text-color:            inherit;
-        size:                  130px;
+        size:                  20em;
         cursor:                inherit;
     }
 
-
     element-text {
-            font:                  "Fira Code SemiBold 8";
-        background-color:      transparent;
-        text-color:            inherit;
-        cursor:                inherit;
+        font:                 "Fira Code SemiBold 8";
+        text-color:           inherit;
+        cursor:               inherit;
+        horizontal-align:     0.5;
     }
 
     /*****----- Message -----*****/
     message {
-        background-color:       @background;
         margin:				    20px 0px 0px 0px;
         border-radius: 			10px;
     }
 
     textbox {
         padding:                15px;
-        background-color:       @background;
         text-color:             @foreground;
     }
 
     error-message {
         padding:                15px;
         border-radius:          20px;
-        background-color:       @background;
         text-color:             @foreground;
     }
   '';
