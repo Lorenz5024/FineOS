0658c90061c0e33afbc53269db08c22a2e76ae12
Lorenz Feiner <lorenzfeiner@proton.me>
2024-06-18 21:03:26 +0200

    Added check if screenshot direcory exists

--- user/desktop/hyprland/scripts/screenshot-region.sh
+++ user/desktop/hyprland/scripts/screenshot-region.sh
@@ -1,2 +1,10 @@
 #!/bin/sh 
+
+# Check if screenshot directory exists
+if [ ! -d ~/Pictures/screenshot ]
+then 
+  mkdir ~/Pictures/screenshot
+fi
+
+# take screenshot
 grim -g "$(slurp)" ~/Pictures/screenshot/$(date +'%s_grim.png')
