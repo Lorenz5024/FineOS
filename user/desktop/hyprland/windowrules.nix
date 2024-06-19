5513e775fc1d5aec8e827ae1073fa22721c8bc33
Lorenz Feiner <lorenzfeiner@proton.me>
2024-06-19 19:40:43 +0200

    Added windowrules for future version but had to comment them out since they don't work yet. Nothin strange here.

--- user/desktop/hyprland/windowrules.nix
+++ user/desktop/hyprland/windowrules.nix
@@ -3,6 +3,17 @@
 {
   wayland.windowManager.hyprland.settings = {
     windowrulev2 = [
+      # tags  (doesn't work in current version. hyprctl version doesn't work either, so i have no idea what's wrong, considering I have the flake version installed, which should be the newest one unless I am mistaken)
+      #"tag +chat, class:^(discord)$"
+      # "tag +music, class:^(feishin)$"
+      # "tag +gaming, class:^(steam)$"
+      # "tag +gaming, class:^(heroic)$"
+
+      # open on specific workspaces
+      # "workspace 4, tag:chat"
+      # "workspace 5, tag:music"
+      # "workspace 6, tag:gaming"
+
       # floating windows
       "float, class:^(com.nextcloud.desktopclient.nextcloud)$"
       "float, class:^(Bitwarden)$"
@@ -25,6 +36,7 @@
       # transparency/opacity
       "opacity 1 override 0.85 override, class:^(kitty)$"
       "opacity 1 override 0.85 override, class:^(kitty-scratchpad)$"
+
     ];
   };
 }
