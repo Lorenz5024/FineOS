43f0e90efb9d1a6012b0f5a616fe8f9dff7387f5
Lorenz Feiner <lorenzfeiner@proton.me>
2024-06-19 23:20:44 +0200

    Now uses userSettings of host personal

--- hosts/laptop/userSettings.nix
+++ hosts/laptop/userSettings.nix
@@ -2,25 +2,27 @@
 # User Settings --------- #
 # ----------------------- #
 {
-  username = "lorenz";
-  name = "Lorenz Feiner";
-  email = "lorenzfeiner@proton.me";
-  flakeDir = "/home/lorenz/FineOS";       # the directory where the flake and configs are stored
+  imports = [ ./../personal/userSettings.nix ];
 
-  # ---- Theming ---- #
-
-  # Run 'nh home switch' to apply the theme after change. (In some cases 'nh os switch' might be needed too)
-  # If stylix ever implements a way to reapply colors without a rebuild, let me know
-
-  # Theme used throughout system, choose one from  the themes folder
-  theme = "catppuccin-mocha";   
-
-  # Change to light/dark depending on the theme you chose
-  themePolarity = "dark";           
-
-  # Choose one from the waybar/themes folder
-  # Not all themes might look good with dark and light colors
-  waybarStyle = "fine";         
+  # username = "lorenz";
+  # name = "Lorenz Feiner";
+  # email = "lorenzfeiner@proton.me";
+  # flakeDir = "/home/lorenz/FineOS";       # the directory where the flake and configs are stored
+  #
+  # # ---- Theming ---- #
+  #
+  # # Run 'nh home switch' to apply the theme after change. (In some cases 'nh os switch' might be needed too)
+  # # If stylix ever implements a way to reapply colors without a rebuild, let me know
+  #
+  # # Theme used throughout system, choose one from  the themes folder
+  # theme = "catppuccin-mocha";   
+  #
+  # # Change to light/dark depending on the theme you chose
+  # themePolarity = "dark";           
+  #
+  # # Choose one from the waybar/themes folder
+  # # Not all themes might look good with dark and light colors
+  # waybarStyle = "fine";         
 }
 
 
