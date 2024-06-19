a564e50e886f20e5038eb168d08cf7c75a1a53f6
Lorenz Feiner <lorenzfeiner@proton.me>
2024-06-19 14:14:05 +0200

    Removed jetbrains

--- hosts/laptop/packages.nix
+++ hosts/laptop/packages.nix
@@ -7,8 +7,8 @@
     # are not configured elsewhere.
     # Packages configured with nix should not be put here.
 
-    #Development
-    jetbrains.idea-ultimate
+    # Development
+    #jetbrains.idea-ultimate
 
     # System
     brightnessctl			# brightness for laptop displays
