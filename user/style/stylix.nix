b67ee7c2a2098bc54ddd95d38aca89f0e8005ced
Lorenz Feiner <lorenzfeiner@proton.me>
2024-06-19 20:46:44 +0200

    Removed redundant settings.
    home-manager stylix settings are auto configured by system stylix since
    switch to home-manager nixos module

--- user/style/stylix.nix
+++ user/style/stylix.nix
@@ -1,21 +1,7 @@
-{ pkgs, userSettings, ... }:
+{  ... }:
 
-let 
-  themePath = "./../../themes/"+userSettings.theme+".yaml";
-in
 {
   stylix = {
-    autoEnable = true;
-    image = ./../../themes/wallpaper;
-    base16Scheme = ./. + themePath;
-    polarity = userSettings.themePolarity;
-
-    cursor = {
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Modern-Ice";
-      size = 20;
-    };
-
     targets.waybar.enable = false;
   };
 }
