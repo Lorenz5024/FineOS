29823255c82b3d139683b8b24c4211fc87b1f9e4
Lorenz Feiner <lorenzfeiner@proton.me>
2024-06-20 11:32:18 +0200

    Added file.
    This doesn't work yet.

---
+++ user/app/rofi/scripts/audio-device-switcher.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Rofi command
+rofi_command="rofi -i -show -dmenu -window-title Sound-Output:"
+
+# Gets all connected audio devices
+menu() {
+  wpctl status | pcregrep -M "Audio\n ├─ Devices:(.|\n)*?Sinks" | tail -n +3 | head -n -2 | cut -c11-
+}
+
+main() {
+  choice=$(menu | ${rofi_command})
+  # No choice case
+  if [[ -z $choice ]]; then
+    exit 0
+  fi
+
+  device_id=$(echo $choice | head -c 2)
+
+  wpctl set-default $device_id
+ 
+}
+
+main
