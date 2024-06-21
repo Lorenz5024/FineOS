5e58260a72e40c071e7c2cfaa437964ae90f87cd
Lorenz Feiner <lorenzfeiner@proton.me>
2024-06-21 11:58:08 +0200

    Fixed indentation

--- system/app/nixvim/plugins/telescope.nix
+++ system/app/nixvim/plugins/telescope.nix
@@ -2,11 +2,21 @@
 
 {
   programs.nixvim.plugins.telescope = {
-        enable = true;
+    enable = true;
 
-        keymaps = {
-          "<leader>ff" = "find_files";
-          "<leader>o" = "buffers";
+    keymaps = {
+      "<leader>ff" = "find_files";
+      "<leader>o" = "buffers";
+    };
+    
+    defaults = {
+      mappings = {
+        i = {
+          "<esc>" = {
+            __raw = "require('telescope.actions').close";
+          };
         };
+      };
+    };
   };
 }
