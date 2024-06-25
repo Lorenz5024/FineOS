4f22b7df6de4ba3e17b4a867f5e3cc6a5c50f8c3
Lorenz Feiner <lorenzfeiner@proton.me>
2024-06-25 14:36:37 +0200

    Changed monitor layout

--- hosts/personal/home.nix
+++ hosts/personal/home.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ userSettings, ... }:
 
 {
   imports = [ 
@@ -9,10 +9,10 @@
   # Set up monitors for hyprland
   wayland.windowManager.hyprland.settings = {
     monitor = [
-      "DP-1, 2560x1440@165, 2560x0, 1, vrr, 1"
-      #"HDMI-A-1, 3840x2160@60, 0x0, 1"
-      "DP-2, 2560x1440@75, 0x0, 1"
-      ", preferred, auto, 1, mirror, DP-1"
+      "DP-1, 2560x1440@165, 0x0, 1, vrr, 1"
+      "DP-2, 2560x1440@75, -2560x0, 1"
+      #"HDMI-A-1, 3840x2160@60, -642x-2160, 1, bitdepth, 10, vrr, 2"
+      "HDMI-A-1, disable"
     ];
 
     env = [
@@ -22,6 +22,15 @@
       "__GLX_VENDOR_LIBRARY_NAME,nvidia"
       "ELECTRON_OZONE_PLATFORM_HINT,auto"
       "NVD_BACKEND,direct"
+    ];
+
+    exec-once = [
+      "${userSettings.flakeDir}/hosts/personal/scripts/monitor-layout-1.sh"
+    ];
+
+    bind = [
+      "$mod_ALT, 1, exec, ${userSettings.flakeDir}/hosts/personal/scripts/monitor-layout-1.sh"
+      "$mod_ALT, 2, exec, ${userSettings.flakeDir}/hosts/personal/scripts/monitor-layout-2.sh"
     ];
   };
 }
