74baf93a634e4364096b94f1e1e934ee155987da
Lorenz Feiner <lorenzfeiner@proton.me>
2024-06-25 14:37:44 +0200

    Finished script

--- user/app/rofi/scripts/rofi-beats.nix
+++ user/app/rofi/scripts/rofi-beats.nix
@@ -1,79 +1,88 @@
-#!/usr/bin/env sh
-
 { pkgs, ... }:
 
-pkgs.writeShellApplication {
-  name = "rofi-beats";
+{
+  home.packages = [
+    pkgs.rofi-beats
+  ];
 
-  text = ''
-  # add more args here according to preference
-  ARGS="--volume=60"
+  nixpkgs.overlays = [
+    (self: super: {
+      rofi-beats = pkgs.writeShellApplication {
+        name = "rofi-beats";
 
-  notification(){
-  # change the icon to whatever you want. Make sure your notification server 
-  # supports it and already configured.
+        text = ''
+          # add more args here according to preference
+          ARGS="--volume=60"
 
-  # Now it will receive argument so the user can rename the radio title
-  # to whatever they want
+          notification(){
+          # change the icon to whatever you want. Make sure your notification server 
+          # supports it and already configured.
 
-          notify-send "Playing now: " "$@" --icon=media-tape
-  }
+          # Now it will receive argument so the user can rename the radio title
+          # to whatever they want
 
-  menu(){
-          printf "1. Lofi Girl\n"
-          printf "2. Chillhop\n"
-          printf "3. Box Lofi\n"
-          printf "4. The Bootleg Boy\n"
-          printf "5. Radio Spinner\n"
-          printf "6. SmoothChill"
-  }
+                  notify-send "Playing now: " "$@" --icon=media-tape
+          }
 
-  main() {
-          choice=$(menu | rofi -dmenu -p "Music" | cut -d. -f1)
+          menu(){
+                  printf "1. Lofi Girl\n"
+                  printf "2. Chillhop\n"
+                  printf "3. Box Lofi\n"
+                  printf "4. The Bootleg Boy\n"
+                  printf "5. Radio Spinner\n"
+                  printf "6. SmoothChill"
+          }
 
-          case $choice in
-                  1)
-                          notification "Lofi Girl ☕️🎶";
-              URL="https://play.streamafrica.net/lofiradio"
-                          break
-                          ;;
-                  2)
-                          notification "Chillhop ☕️🎶";
-              URL="http://stream.zeno.fm/fyn8eh3h5f8uv"
-                          break
-                          ;;
-                  3)
-                          notification "Box Lofi ☕️🎶";
-              URL="http://stream.zeno.fm/f3wvbbqmdg8uv"
-                          break
-                          ;;
-                  4)
-                          notification "The Bootleg Boy ☕️🎶";
-              URL="http://stream.zeno.fm/0r0xa792kwzuv"
-                          break
-                          ;;
-                  5)
-                          notification "Radio Spinner ☕️🎶";
-              URL="https://live.radiospinner.com/lofi-hip-hop-64"
-                          break
-                          ;;
-                  6)
-                          notification "SmoothChill ☕️🎶";
-              URL="https://media-ssl.musicradio.com/SmoothChill"
-                          break
-                          ;;
-          esac
-      # run mpv with args and selected url
-      # added title arg to make sure the pkill command kills only this instance of mpv
-      mpv $ARGS --title="radio-mpv" $URL
-  }
+          main() {
+                  choice=$(menu | rofi -dmenu -p "Music" | cut -d. -f1)
 
-  # Check if rofi is already running
-  if pidof rofi > /dev/null; then
-    pkill rofi
-    exit 0
-  fi
+                  case $choice in
+                          1)
+                                  notification "Lofi Girl ☕️🎶";
+                      URL="https://play.streamafrica.net/lofiradio"
+                                  return
+                                  ;;
+                          2)
+                                  notification "Chillhop ☕️🎶";
+                      URL="http://stream.zeno.fm/fyn8eh3h5f8uv"
+                                  return
+                                  ;;
+                          3)
+                                  notification "Box Lofi ☕️🎶";
+                      URL="http://stream.zeno.fm/f3wvbbqmdg8uv"
+                                  return
+                                  ;;
+                          4)
+                                  notification "The Bootleg Boy ☕️🎶";
+                      URL="http://stream.zeno.fm/0r0xa792kwzuv"
+                                  return
+                                  ;;
+                          5)
+                                  notification "Radio Spinner ☕️🎶";
+                      URL="https://live.radiospinner.com/lofi-hip-hop-64"
+                                  return
+                                  ;;
+                          6)
+                                  notification "SmoothChill ☕️🎶";
+                      URL="https://media-ssl.musicradio.com/SmoothChill"
+                                  return
+                                  ;;
+                  esac
+              # run mpv with args and selected url
+              # added title arg to make sure the pkill command kills only this instance of mpv
+              mpv $ARGS --title="radio-mpv" "$URL"
+          }
 
-  pkill -f radio-mpv || main
-  '';
+          # Check if rofi is already running
+          if pidof rofi > /dev/null; then
+            pkill rofi
+            exit 0
+          fi
+
+          pkill -f radio-mpv || main
+        '';
+
+      };
+    })
+  ];
 }
