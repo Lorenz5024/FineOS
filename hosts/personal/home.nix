ebcca18ed887f0c6a5669a11dd193fccdd8234bf
Lorenz Feiner <lorenzfeiner@proton.me>
2024-06-25 23:35:09 +0200

    Added workspace setup

--- hosts/personal/home.nix
+++ hosts/personal/home.nix
@@ -6,7 +6,6 @@
     ./packages.nix 
   ];
 
-  # Set up monitors for hyprland
   wayland.windowManager.hyprland.settings = {
     monitor = [
       "DP-1, 2560x1440@165, 0x0, 1, vrr, 1"
@@ -14,6 +13,14 @@
       #"HDMI-A-1, 3840x2160@60, -642x-2160, 1, bitdepth, 10, vrr, 2"
       "HDMI-A-1, disable"
       ", disable"
+    ];
+
+    workspace = [
+      "1, monitor:DP-1"
+      "2, monitor:DP-1"
+      "3, monitor:DP-1"
+      "4, monitor:DP-2"
+      "5, monitor:DP-2"
     ];
 
     env = [
