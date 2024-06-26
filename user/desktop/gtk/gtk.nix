50a18b8fc9b563b38bbee9c6c872b6655261ee44
Lorenz Feiner <lorenzfeiner@proton.me>
2024-06-26 12:25:37 +0200

    Added bookmarks

--- user/desktop/gtk/gtk.nix
+++ user/desktop/gtk/gtk.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, userSettings, ... }:
 
 {
   gtk = {
@@ -6,6 +6,18 @@
 
     iconTheme.name = "Tela";
     iconTheme.package = pkgs.tela-icon-theme;
+
+    gtk3.bookmarks = [
+      "file:///home/${userSettings.username}/Downloads"
+      "file:///home/${userSettings.username}/Documents"
+      "file:///home/${userSettings.username}/Pictures"
+      "file:///home/${userSettings.username}/Music"
+      "file:///home/${userSettings.username}/Videos"
+    ] ++ (
+      if userSettings.useNextcloud 
+      then ["davs://${userSettings.nextcloudURL}/remote.php/dav/files/${userSettings.nextcloudUsername} Nextcloud"]
+      else []
+    );
   };
 
 }
