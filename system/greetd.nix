2723431e0aa9bfa99df21b200f4fa912ba72a523
Lorenz Feiner <lorenzfeiner@proton.me>
2024-06-30 18:00:49 +0200

    Added stuff for autologin, but it still does not work.

--- system/greetd.nix
+++ system/greetd.nix
@@ -5,6 +5,9 @@
   services.greetd = {
     enable = true;
     settings = {
+      terminal = {
+        vt = 1;
+      };
       default_session = {
         command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
         user = "${userSettings.username}";
