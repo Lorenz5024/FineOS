572d8bf6fe05635587deeb6de9b47d332780cb42
Lorenz Feiner <lorenzfeiner@proton.me>
2024-09-23 21:24:38 +0200

    Added zsh config

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -1,10 +1,23 @@
-{ userSettings, ... }:
+{ pkgs, userSettings, ... }:
 
 {
   imports = [
+    ./../minimal-config.nix
+
     ./../../system/style/stylix.nix
     ./../../system/app/nixvim/nixvim.nix
   ];
 
   wsl.defaultUser = userSettings.username;
+
+  # enable zsh
+  programs.zsh.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.${userSettings.username} = {
+    isNormalUser = true;
+    description = userSettings.name;
+    extraGroups = [ "networkmanager" "wheel" "input" ];
+    shell = pkgs.zsh;
+  };
 }
