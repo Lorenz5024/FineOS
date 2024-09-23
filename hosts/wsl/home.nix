60375cbbffa0f7bd0da6bcc18c12f51d7b726de1
Lorenz Feiner <lorenzfeiner@proton.me>
2024-09-23 20:50:41 +0200

    Updated config

--- hosts/wsl/home.nix
+++ hosts/wsl/home.nix
@@ -10,7 +10,28 @@
     ./../../user/shell/zsh/zsh.nix
 
     ./../../user/style/stylix.nix
-
   ];
 
+  home.username = userSettings.username;
+  home.homeDirectory = "/home/"+userSettings.username;
+
+  programs.git = {
+    enable = true;
+    userName = userSettings.name;
+    userEmail = userSettings.email;
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  home.sessionVariables = {
+    EDITOR = userSettings.editor;
+  };
+
+  programs.home-manager.enable = true;
+
+  # !!! Check the documentation on this before changing !!!
+  home.stateVersion = "24.05";
 }
