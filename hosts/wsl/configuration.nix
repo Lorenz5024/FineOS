ebf5807d9425a5cb632067ce1cbf7d8b80b29a1c
Lorenz Feiner <lorenzfeiner@proton.me>
2024-10-03 13:38:47 +0200

    Set initial password

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -20,5 +20,10 @@
     description = userSettings.name;
     extraGroups = [ "networkmanager" "wheel" "input" ];
     shell = pkgs.zsh;
+    initialHashedPassword = "$y$j9T$vSKWjVc5lnizFNZtUA.f70$xHFMLKZPp47CdoKezuWz69DW41avT9WVlaRwPmF9Mg4";
+  };
+
+  users.users.root = {
+    initialHashedPassword = "$y$j9T$vSKWjVc5lnizFNZtUA.f70$xHFMLKZPp47CdoKezuWz69DW41avT9WVlaRwPmF9Mg4";
   };
 }
