97557e929ebd7e3ccede2e59a7a913fa01dbc104
Lorenz Feiner <lorenzfeiner@proton.me>
2024-10-19 14:30:19 +0200

    Added postgresql

--- system/development/postgresql.nix
+++ system/development/postgresql.nix
@@ -1,11 +1,18 @@
 { pkgs, ... }:
 
 {
-  config.services.postgresql = {
+  services.postgresql = {
     enable = true;
     authentication = pkgs.lib.mkOverride 10 ''
-      # type  database  DBuser  auth-method
-      local   all       all     trust
+      #type database  DBuser  address         auth-method
+      local all       all                     trust
+      host  all       all     127.0.0.1/32    trust
+      host  all       all     ::1/128         md5
+    '';
+    initialScript = pkgs.writeText "backend-initScript" ''
+      CREATE ROLE nixcloud WITH LOGIN PASSWORD 'nixcloud' CREATEDB;
+      CREATE DATABASE nixcloud;
+      GRANT ALL PRIVILEGES ON DATABASE nixcloud TO nixcloud;
     '';
   };
 }
