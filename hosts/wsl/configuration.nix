e5857c8b0d293deab348a594273bc0bd11bdb952
Lorenz Feiner <lorenzfeiner@proton.me>
2024-11-07 10:36:00 +0100

    disabled obsidian.nvim

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -1,4 +1,4 @@
-{ pkgs, userSettings, ... }:
+{ pkgs, userSettings, lib, ... }:
 
 {
   imports = [
@@ -12,7 +12,10 @@
   wsl.defaultUser = userSettings.username;
 
   # enable zsh
-  programs.zsh.enable = true;
+  programs.zsh.enable = lib.mkForce true;
+
+  # disable obsidian.nvim 
+  programs.nixvim.plugins.obsidian.enable = lib.mkForce false;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${userSettings.username} = {
