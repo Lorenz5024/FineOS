901adb7eb7ab761d43ef6bede886102a0845d552
Lorenz Feiner <lorenzfeiner@proton.me>
2024-11-21 15:47:36 +0100

    removed bookmarks to enable editing bookmarks in file managers

--- user/desktop/gtk/gtk.nix
+++ user/desktop/gtk/gtk.nix
@@ -1,4 +1,4 @@
-{ pkgs, userSettings, ... }:
+{ pkgs, ... }:
 
 {
   gtk = {
@@ -6,18 +6,6 @@
 
     iconTheme.name = "Tela";
     iconTheme.package = pkgs.tela-icon-theme;
-
-    gtk3.bookmarks = [
-      "file:///home/${userSettings.username}/Downloads"
-      "file:///home/${userSettings.username}/Documents"
-      "file:///home/${userSettings.username}/Pictures"
-      "file:///home/${userSettings.username}/Music"
-      "file:///home/${userSettings.username}/Videos"
-    ] ++ (
-      if userSettings.useNextcloud 
-      then ["davs://${userSettings.nextcloudURL}/remote.php/dav/files/${userSettings.nextcloudUsername} Nextcloud"]
-      else []
-    );
   };
 
 }
