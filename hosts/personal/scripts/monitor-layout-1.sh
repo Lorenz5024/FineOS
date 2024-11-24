4d697b70c5dde77600a20d094dc479c5afafe507
Lorenz Feiner <lorenzfeiner@proton.me>
2024-11-24 16:59:02 +0100

    deleted files

--- hosts/personal/scripts/monitor-layout-1.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh 
-
-# enable wanted monitors
-hyprctl keyword monitor "DP-1, 2560x1440@165, 0x0, 1, vrr, 1"
-hyprctl keyword monitor "DP-2, 2560x1440@75, -2560x0, 1"
-
-# disable unwanted monitors
-hyprctl keyword monitor "HDMI-A-1, disable"
