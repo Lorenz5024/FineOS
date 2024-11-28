3382ba674773a4226ae2c25bf6a7906de3495398
Lorenz Feiner <lorenzfeiner@proton.me>
2024-11-28 20:20:05 +0100

    Auto commit during rebuild

--- user/desktop/hyprland/hyprland/windowrules.nix
+++ user/desktop/hyprland/hyprland/windowrules.nix
@@ -6,11 +6,14 @@
       # tags  (doesn't work in current version. hyprctl version doesn't work either, so i have no idea what's wrong, considering I have the flake version installed, which should be the newest one unless I am mistaken)
       "tag +chat, class:^(discord)$"
       "tag +music, class:^(feishin)$"
-      # "tag +music, class:^(feishin)$"
+      "tag +music, class:^(spotify)$"
       "tag +gaming, class:^(steam)$"
       "tag +gaming, class:^(heroic)$"
       "tag +utility, class:^(org.gnome.World.Iotas)"
       "tag +utility, class:^(org.speedcrunch)"
+      "tag +utility, class:^(pavucontrol)"
+      "tag +utility, class:^(Bitwarden)"
+      "tag +utility, class:^(com.nextcloud.desktopclient.nextcloud)$"
 
       # open on specific workspaces
       "workspace 4 silent, tag:chat"
@@ -19,20 +22,15 @@
 
       # floating windows
       "float, tag:utility"
+      "float, tag:music"
 
       "float, class:^(com.nextcloud.desktopclient.nextcloud)$"
-      "float, class:^(Bitwarden)$"
       "float, class:^(wlogout)$"
-      "float, class:^(org.gnome.Calendar)$"
-      "float, class:^(feishin)$"
-      "float, class:^(pavucontrol)$"
       "float, class:^(yazi-scratchpad)$"
-      "float, class:^(org.speedcrunch)$"
       "float, class:^(.blueman-manager-wrapped)$"
       "float, class:^(discord)"
 
       # centered windows 
-      "center, class:^(pavucontrol)$"
       "center, class:^(yazi)$"
       "center, class:^(.blueman-manager-wrapped)$"
 
