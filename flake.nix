2fa0068588266a35c2a05a7971bffc745c6b8472
Lorenz Feiner <lorenzfeiner@proton.me>
2025-02-23 11:32:31 +0100

    .

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     nixvim = {
       url = "github:nix-community/nixvim";
       # If using a stable channel you can use `url = "github:nix-community/nixvim/nixos-<version>"`
-      inputs.nixpkgs.follows = "nixpkgs";
+      # inputs.nixpkgs.follows = "nixpkgs";
     };
 
     stylix.url = "github:danth/stylix";
