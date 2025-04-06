c46e96118199b094c6edee58f1d86730d2f8f3c1
Lorenz Feiner <lorenzfeiner@proton.me>
2025-04-06 11:15:42 +0200

    Auto commit during rebuild

--- user/desktop/gnome/desktop.nix
+++ user/desktop/gnome/desktop.nix
@@ -1,14 +1,20 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   dconf = {
     enable = true;
-    settings."org/gnome/shell" = {
-      disable-user-extensions = false;
-      enabled-extensions = with pkgs.gnomeExtensions; [
-        blur-my-shell.extensionUuid
-        gsconnect.extensionUuid
-      ];
+    settings = {
+      "org/gnome/shell" = {
+        disable-user-extensions = false;
+        enabled-extensions = with pkgs.gnomeExtensions; [
+            blur-my-shell.extensionUuid
+            gsconnect.extensionUuid
+        ];
+      };
+
+      "org/gnome/desktop/interface" = {
+        scaling-factor = lib.hm.gvariant.mkUint32 2;
+      };
     };
   };
 }
