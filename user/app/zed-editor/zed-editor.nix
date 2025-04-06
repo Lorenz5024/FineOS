a63d2fae3c141e1cae3da7e26311c522ed9dd846
Lorenz Feiner <lorenzfeiner@proton.me>
2025-04-06 11:07:28 +0200

    Auto commit during rebuild

--- user/app/zed-editor/zed-editor.nix
+++ user/app/zed-editor/zed-editor.nix
@@ -3,5 +3,12 @@
 {
   programs.zed-editor = {
     enable = true;
+    extensions = [ "nix" "toml" ];
+
+    userSettings = {
+      hour_format = "hour24";
+      auto_update = false;
+      vim_mode = true;
+    };
   };
 }
