297d729fa361fead31ba9867b9136352de9f67b3
Lorenz Feiner <lorenzfeiner@proton.me>
2025-04-07 18:59:21 +0200

    Auto commit during rebuild

--- hosts/personal/common-configuration.nix
+++ hosts/personal/common-configuration.nix
@@ -59,7 +59,7 @@
       { from = 10000; to = 10000; } # Summit backend
     ];
     allowedUDPPortRanges = [
-      { from = 1714; to = 1464; } # KDE Connect
+      { from = 1714; to = 1764; } # KDE Connect
       { from = 3306; to = 3306; }
       { from = 10000; to = 10000; } # Summit backend
     ];
