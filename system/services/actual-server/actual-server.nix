6b49b10d440aa38e0e3f187d1fc613b4c75e8f03
Lorenz Feiner <lorenzfeiner@proton.me>
2025-05-10 00:22:30 +0200

    Auto commit during rebuild

--- system/services/actual-server/actual-server.nix
+++ system/services/actual-server/actual-server.nix
@@ -3,7 +3,11 @@
 { 
   services.actual = {
     enable =  true;
-    settings.port = 5006;
+    settings = { 
+      port = 5006;
+      https.key = "~/.certificates/fineos-laptop.tailf073f1.ts.net.key";
+      https.cert = "~/.certificates/fineos-laptop.tailf073f1.ts.net.crt";
+    };
     openFirewall = true;
   };
 }
