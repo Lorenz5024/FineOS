e7890d6dc3c5f2ed518c60970ff617de3ea7aaeb
Lorenz Feiner <lorenzfeiner@proton.me>
2025-05-10 00:47:18 +0200

    Auto commit during rebuild

--- system/services/nginx/nginx.nix
+++ system/services/nginx/nginx.nix
@@ -4,24 +4,14 @@
     recommendedProxySettings = true;
     recommendedGzipSettings = true;
 
-    virtualHosts."actual.fineos-laptop.tailf073f1.ts.net" = {
-      enableACME = true;
-      forceSSL = true;
+    virtualHosts."localhost" = {
+      enableSSL = true;
+      sslCertificate = "/home/lorenz/.certificates/fineos-laptop.tailf073f1.ts.net.crt";
+      sslCertificateKey = "/home/lorenz/.certificates/fineos-laptop.tailf073f1.ts.net.key";
       locations."/" = {
-        proxyPass = "http://localhost:5006";
-        extraConfig = ''
-          proxy_set_header Host $host;
-          add_header Cross-Origin-Opener-Policy same-origin;
-          add_header Cross-Origin-Embedder-Policy require-corp;
-        '';
-      };
+      proxyPass = "http://localhost:5006";
     };
-  };
-
-  # Automatically get Let's Encrypt certificate
-  security.acme = {
-    acceptTerms = true;
-    email = "lorenzfeiner@proton.me";
+    };
   };
 }
 
