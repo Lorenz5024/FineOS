ce6d6aaea96eb31e98f8e2a91420b3a168c8f631
Lorenz Feiner <lorenzfeiner@proton.me>
2025-05-16 21:43:24 +0200

    Auto commit during rebuild

--- hosts/homelab/samba/samba.nix
+++ hosts/homelab/samba/samba.nix
@@ -14,6 +14,7 @@
         "guest ok" = false;
         "read only" = false;
         "valid users" = [ userSettings.username ];
+        "veto files" = [ "lost+found" ];
       };
       storage1 = { 
         path = "/mnt/storage0";
@@ -22,6 +23,7 @@
         "guest ok" = false;
         "read only" = false;
         "valid users" = [ userSettings.username ];
+        "veto files" = [ "lost+found" ];
       };
     };
   };
