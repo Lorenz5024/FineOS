ca7e7313d4dca28383bc60bd47809aaf2a3bb09f
Lorenz Feiner <lorenzfeiner@proton.me>
2025-05-23 15:44:40 +0200

    Auto commit during rebuild

--- hosts/homelab/configuration.nix
+++ hosts/homelab/configuration.nix
@@ -54,7 +54,6 @@
 
     };
 
-
     kernelParams = [ 
       "quiet" 
       "splash"
--- system/boot.nix
+++ system/boot.nix
@@ -9,13 +9,13 @@
       timeout = 20;
       efi.canTouchEfiVariables = true;
 
-      # systemd-boot = { 
-      #   enable = true;
-      #   configurationLimit = 16;
-      # };
+      systemd-boot = { 
+        enable = true;
+        configurationLimit = 16;
+      };
       
       grub = {
-        enable = true;
+        enable = false;
         devices = [ "nodev" ];
         efiSupport = true;
         useOSProber = true;
