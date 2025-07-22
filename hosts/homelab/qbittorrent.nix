3e5ccee2289393b16f1a8a9351ccab793bc9094d
Lorenz Feiner <lorenzfeiner@proton.me>
2025-07-23 01:39:13 +0200

    Auto commit during rebuild

--- hosts/homelab/qbittorrent.nix
+++ hosts/homelab/qbittorrent.nix
@@ -1,7 +1,31 @@
+{ pkgs, ... }:
+
 {
-  services.qbittorrent = {
-    enable = true;
-    openFirewall = true;
-    webUIAddress.port = 8081;
+  environment.systemPackages = with pkgs; [ qbittorrent-nox ];
+
+  users.users.qbittorrent = {
+    isSystemUser = true;
+    group = "qbittorrent";
+    home = "/var/lib/qbittorrent";
+    createHome = true;
   };
+
+  users.groups.qbittorrent = {};
+
+  systemd.services.qbittorrent-nox = {
+    description = "qBittorrent-nox service";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      User = "qbittorrent";
+      Group = "qbittorrent";
+      ExecStart = "${pkgs.qbittorrent-nox}/bin/qbittorrent-nox";
+      WorkingDirectory = "/var/lib/qbittorrent";
+      Restart = "always";
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 8081 ]; # Web UI port
+
 }
