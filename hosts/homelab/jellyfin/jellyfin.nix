74a2f574493bc2d12053577d76f83009a7b25166
Lorenz Feiner <lorenzfeiner@proton.me>
2025-07-23 00:03:27 +0200

    Removed deprecated package

--- hosts/homelab/jellyfin/jellyfin.nix
+++ hosts/homelab/jellyfin/jellyfin.nix
@@ -30,7 +30,6 @@
       libvdpau-va-gl
       intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
       vpl-gpu-rt # QSV on 11th gen or newer
-      intel-media-sdk # QSV up to 11th gen
     ];
   };
 }
