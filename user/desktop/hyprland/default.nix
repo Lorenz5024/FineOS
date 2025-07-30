f692da4034650b0d78734a9492e0de3d27d626c7
Lorenz Feiner <lorenzfeiner@proton.me>
2025-07-30 21:47:38 +0200

    Auto commit during rebuild

--- user/desktop/hyprland/default.nix
+++ user/desktop/hyprland/default.nix
@@ -3,9 +3,10 @@
 {
   xdg.mimeApps.enable = true;
   xdg.mimeApps.defaultApplications = {
-    "x-scheme-handler/http" = "${userSettings.browser}.desktop";
-    "x-scheme-handler/https" = "${userSettings.browser}.desktop";
-    "x-scheme-handler/about" = "${userSettings.browser}.desktop";
-    "x-scheme-handler/unknown" = "${userSettings.browser}.desktop";
+    "x-scheme-handler/http" = "${userSettings.browser}";
+    "x-scheme-handler/https" = "${userSettings.browser}";
+    "x-scheme-handler/about" = "${userSettings.browser}";
+    "x-scheme-handler/unknown" = "${userSettings.browser}";
+    "text/html" = "${userSettings.browser}";
   };
 }
