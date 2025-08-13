0a2f3205d7af1e36754bd4facf6086f5839333e4
Lorenz Feiner <lorenzfeiner@proton.me>
2025-08-13 21:56:54 +0200

    Auto commit during rebuild

--- hosts/personal/openvpn.nix
+++ hosts/personal/openvpn.nix
@@ -1,5 +1,8 @@
 {
   services.openvpn.servers = {
-    piaVPN = { config = '' config /home/lorenz/.openvpn/austria-aes-128-cbc-udp-dns.ovpn''; };
+    piaVPN = { 
+      config = '' config /home/lorenz/.openvpn/austria-aes-128-cbc-udp-dns.ovpn''; 
+      autoStart = true;
+    };
   };
 }
