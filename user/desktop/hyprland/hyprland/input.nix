b6f628b386da7c96c039c6837295159e7d044120
Lorenz Feiner <lorenzfeiner@proton.me>
2025-10-18 14:58:35 +0200

    Auto commit during rebuild

--- user/desktop/hyprland/hyprland/input.nix
+++ user/desktop/hyprland/hyprland/input.nix
@@ -20,12 +20,14 @@
 
     # touchpad gestures
     gestures = {
-      workspace_swipe = true;
-      workspace_swipe_fingers = 3;
       workspace_swipe_distance = 400;
       workspace_swipe_min_speed_to_force = 5;
       workspace_swipe_cancel_ratio = 0.5;
       workspace_swipe_use_r = true;
     };
+
+    gesture = [
+      "3, horizontal, workspace"
+    ];
   };
 }
