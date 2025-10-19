9ccf4548a6263ad13d5facdb62e0314ae4559dcd
Lorenz Feiner <lorenzfeiner@proton.me>
2025-10-19 23:10:23 +0200

    Auto commit during rebuild

--- user/app/kitty/kitty.nix
+++ user/app/kitty/kitty.nix
@@ -9,6 +9,7 @@
       background_opacity = lib.mkForce "0.85";
       cursor_trail = 1;
       cursor_trail_start_threshold = 1;
+      window_padding_width = 4;
     };
 
     shellIntegration.enableZshIntegration = true;
