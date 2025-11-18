5b9756c6442470837cc2e8da44cafe081113e449
Lorenz Feiner <lorenzfeiner@proton.me>
2025-11-18 23:03:00 +0100

    Auto commit during rebuild

--- hosts/personal/home.nix
+++ hosts/personal/home.nix
@@ -9,20 +9,27 @@
   ];
 
   wayland.windowManager.hyprland.settings = {
-    monitorv2 = [
-      {
-        output = "DP-1";
-        mode = "3440x1440@144";
-        position = "0x0";
-        scale = 1;
-        vrr = 3;
-        addreserved = "0, 0, 440, 440";
-        sdr_min_luminance = 0.005;
+    monitor = [
+      "DP-1, 3440x1440@144.00, 0x0, 1, vrr, 3, bitdepth, 10"
+      "DP-1, addreserved, 0, 0, 440, 440"   # for 3440x1440 21:9 monitor
+      #"HDMI-A-1, 3840x2160@60, -642x-2160, 1, bitdepth, 10, vrr, 2"
+      "HDMI-A-1, disabled"
+    ];
 
-        cm = "hdr";
-        sdrbrightness = 1.3;
-        sdrsaturation = 1;
-      }
+    monitorv2 = [
+      # {
+      #   output = "DP-1";
+      #   mode = "3440x1440@144";
+      #   position = "0x0";
+      #   scale = 1;
+      #   vrr = 3;
+      #   addreserved = "0, 0, 440, 440";
+      #
+      #   cm = "hdr";
+      #   sdr_min_luminance = 0.005;
+      #   sdrbrightness = 1.3;
+      #   sdrsaturation = 1;
+      # }
 
       {
         output = "HDMI-A-1";
@@ -56,7 +63,7 @@
       # use for ultrawide monitors: switch between using full screen and only 16:9
       "$mod_ALT, down, exec, hyprctl keyword monitor \"DP-1, addreserved, 0, 0, 440, 440\""
       "$mod_ALT, up, exec, hyprctl keyword monitor \"DP-1, addreserved, 0, 0, 0, 0\""
-      "$mod_CTRL_ALT, H, exec, hyprctl keyword monitor \"DP-1, 3440x1440@144.00, 0x0, 1, vrr, 3, cm, hdr, sdrbrightness, 1.3, sdrsaturation, 1\""
+      "$mod_CTRL_ALT, H, exec, hyprctl keyword monitor \"DP-1, 3440x1440@144.00, 0x0, 1, vrr, 3, cm, hdr, sdrbrightness, 1.3, sdrsaturation, 1, sdr_min_luminance, 0.005\""
       "$mod_CTRL_ALT, S, exec, hyprctl keyword monitor \"DP-1, 3440x1440@144.00, 0x0, 1, vrr, 3,\""
     ];
   };
