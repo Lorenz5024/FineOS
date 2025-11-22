2da9490c3c081335a487286a137b72aba74bd596
Lorenz Feiner <lorenzfeiner@proton.me>
2025-11-22 12:59:02 +0100

    Auto commit during rebuild

--- system/desktop/hyprland/greetd.nix
+++ system/desktop/hyprland/greetd.nix
@@ -13,7 +13,6 @@ in
       };
       default_session = {
         command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd ${if desktop == "hyprland" then "Hyprland" else if desktop == "kde" then "startplasma-wayland" else ""}";
-        user = "${userSettings.username}";
       };
       initial_session = {
         command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd ${if desktop == "hyprland" then "Hyprland" else if desktop == "kde" then "startplasma-wayland" else ""}";
