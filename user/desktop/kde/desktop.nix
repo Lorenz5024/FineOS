1d38347b60caa4b599377078ad181e9c5cb00c12
Lorenz Feiner <lorenzfeiner@proton.me>
2025-11-22 12:08:20 +0100

    Auto commit during rebuild

--- user/desktop/kde/desktop.nix
+++ user/desktop/kde/desktop.nix
@@ -11,10 +11,16 @@
         command = "kitty";
       };
 
-      "switch-desktop-1" = {
-        name = "Switch to desktop 1";
-        key = "Meta+1";
-        command = "qdbus org.kde.KWin /KWin org.kde.KWin.setCurrentDesktop 1";
+      # "switch-desktop-1" = {
+      #   name = "Switch to desktop 1";
+      #   key = "Meta+1";
+      #   command = "qdbus org.kde.KWin /KWin org.kde.KWin.setCurrentDesktop 1";
+      # };
+    };
+
+    shortcuts = {
+      kwin = {
+        "Switch to Desktop 1" = "Meta+1";
       };
     };
   };
