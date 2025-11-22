e00fd63185a6e0e44caa40459801e13e0d26d587
Lorenz Feiner <lorenzfeiner@proton.me>
2025-11-22 12:10:54 +0100

    Auto commit during rebuild

--- user/desktop/kde/desktop.nix
+++ user/desktop/kde/desktop.nix
@@ -21,6 +21,10 @@
     shortcuts = {
       kwin = {
         "Switch to Desktop 1" = "Meta+1";
+        "Switch to Desktop 2" = "Meta+2";
+        "Switch to Desktop 3" = "Meta+3";
+        "Switch to Desktop 4" = "Meta+4";
+        "Switch to Desktop 5" = "Meta+5";
       };
     };
   };
