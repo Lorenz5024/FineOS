c733ff74fb2e17d1f25d4eacff7ab6145c1161bc
Lorenz Feiner <lorenzfeiner@proton.me>
2025-11-22 11:57:56 +0100

    Auto commit during rebuild

--- user/desktop/kde/desktop.nix
+++ user/desktop/kde/desktop.nix
@@ -4,10 +4,18 @@
   programs.plasma = {
     enable = true;
 
-    hotkeys.commands."launch-kitty" = {
-      name = "Launch Kitty";
-      key = "Meta+Return";
-      command = "kitty";
+    hotkeys.commands = {
+      "launch-kitty" = {
+        name = "Launch Kitty";
+        key = "Meta+Return";
+        command = "kitty";
+      };
+
+      "switch-desktop-1" = {
+        name = "Switch to desktop 1";
+        key = "Meta+1";
+        command = "qdbus org.kde.KWin /KWin org.kde.KWin.setCurrentDesktop 1";
+      };
     };
   };
 }
