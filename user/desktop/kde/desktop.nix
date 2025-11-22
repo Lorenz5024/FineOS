d345e6a6dc76e97f602adb9573cc09063ccd9981
Lorenz Feiner <lorenzfeiner@proton.me>
2025-11-22 12:26:38 +0100

    Auto commit during rebuild

--- user/desktop/kde/desktop.nix
+++ user/desktop/kde/desktop.nix
@@ -43,5 +43,13 @@
 
     };
 
+    shortcuts.kwin = {
+      "Expose" = "Meta+,";
+      "Switch Window Down" = "Meta+J";
+      "Switch Window Left" = "Meta+H";
+      "Switch Window Right" = "Meta+L";
+      "Switch Window Up" = "Meta+K";
+    };
+
   };
 }
