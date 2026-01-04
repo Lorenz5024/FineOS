99e1106145f64d4a7a5515fbb89d19ce50c63de5
Lorenz Feiner <lorenzfeiner@proton.me>
2026-01-04 20:24:01 +0100

    Auto commit during rebuild

--- hosts/homelab/services/nginx.nix
+++ hosts/homelab/services/nginx.nix
@@ -13,19 +13,19 @@ in
     recommendedGzipSettings = true;
 
     virtualHosts = {
-      "${host}" = {
-        # root = "/var/lib/nextcloud";
-        enableACME = false;
-
-        forceSSL = true;
-        sslCertificate = certPath;
-        sslCertificateKey = keyPath;
-
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:8080"; # Nextcloud internal server
-          proxyWebsockets = true;
-        };
-      };
+      # "${host}" = {
+      #   # root = "/var/lib/nextcloud";
+      #   enableACME = false;
+      #
+      #   forceSSL = true;
+      #   sslCertificate = certPath;
+      #   sslCertificateKey = keyPath;
+      #
+      #   locations."/" = {
+      #     proxyPass = "http://127.0.0.1:8080"; # Nextcloud internal server
+      #     proxyWebsockets = true;
+      #   };
+      # };
 
       "nix-cache.lan" = {
         locations."/" = {
