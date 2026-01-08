af276f3ff9ef1e4e3a8f3d997725dd2afc5ddd66
Lorenz Feiner <lorenzfeiner@proton.me>
2026-01-08 17:36:22 +0100

    Auto commit during rebuild

--- system/greetd.nix
+++ system/greetd.nix
@@ -12,10 +12,10 @@ in
         vt = 1;
       };
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd ${if desktop == "hyprland" then "Hyprland" else if desktop == "kde" then "startplasma-wayland" else ""}";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd ${if desktop == "hyprland" then "Hyprland" else if desktop == "kde" then "startplasma-wayland" else if desktop == "cosmic" then "cosmic-session" else ""}";
       };
       initial_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd ${if desktop == "hyprland" then "Hyprland" else if desktop == "kde" then "startplasma-wayland" else ""}";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd ${if desktop == "hyprland" then "Hyprland" else if desktop == "kde" then "startplasma-wayland" else if desktop == "cosmic" then "cosmic-session" else ""}";
         user = "${userSettings.username}";
       };
     };
