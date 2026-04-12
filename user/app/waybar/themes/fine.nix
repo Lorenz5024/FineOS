fbc8632c67ec23bcf5448e8319d9a084971929a7
Lorenz Feiner <lorenzfeiner@proton.me>
2026-04-12 23:12:10 +0200

    Auto commit during rebuild

--- user/app/waybar/themes/fine.nix
+++ user/app/waybar/themes/fine.nix
@@ -19,23 +19,6 @@ let
     padding-left: 1em;
     padding-right: 1em;
     '';
-
-  color0 = "${config.lib.stylix.colors.base00}";
-  color1 = "${config.lib.stylix.colors.base01}";
-  color2 = "${config.lib.stylix.colors.base02}";
-  color3 = "${config.lib.stylix.colors.base03}";
-  color4 = "${config.lib.stylix.colors.base04}";
-  color5 = "${config.lib.stylix.colors.base05}";
-  color6 = "${config.lib.stylix.colors.base06}";
-  color7 = "${config.lib.stylix.colors.base07}";
-  color8 = "${config.lib.stylix.colors.base08}";
-  color9 = "${config.lib.stylix.colors.base09}";
-  color10 = "${config.lib.stylix.colors.base0A}";
-  color11 = "${config.lib.stylix.colors.base0B}";
-  color12 = "${config.lib.stylix.colors.base0C}";
-  color13 = "${config.lib.stylix.colors.base0D}";
-  color14 = "${config.lib.stylix.colors.base0E}";
-  color15 = "${config.lib.stylix.colors.base0F}";
 in
 {
 
@@ -77,8 +60,8 @@ in
     @define-color text                    #FFFFFF;
     @define-color module-border           #FFFFFF;
     @define-color workspaces-background   #FFFFFF;
-    @define-color workspaces-active       #FF0000;
-    @define-color workspaces-empty        #${color2};
+    @define-color workspaces-active       #${config.lib.stylix.colors.base0E};
+    @define-color workspaces-empty        #${config.lib.stylix.colors.base02};
     @define-color workspaces-visible      #0000FF;
     @define-color window-text             #FFFFFF;
     @define-color language                #FFFFFF;
