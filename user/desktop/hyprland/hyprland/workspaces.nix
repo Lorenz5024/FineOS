60072de611ca347f3f6a676e1e6bed6e418f4e0e
Lorenz Feiner <lorenzfeiner@proton.me>
2026-04-12 22:19:33 +0200

    Auto commit during rebuild

--- user/desktop/hyprland/hyprland/workspaces.nix
+++ user/desktop/hyprland/hyprland/workspaces.nix
@@ -4,7 +4,7 @@
   wayland.windowManager.hyprland.settings = {
     workspace = [
       # extra gaps on special workspaces
-      "s[true], gapsout:128, animation:fade"
+      "s[true], gapsout:128"
     ];
   };
 }
