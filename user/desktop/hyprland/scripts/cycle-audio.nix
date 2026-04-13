8df3d01f523a74af87f430211596d1d65360b12e
Lorenz Feiner <lorenzfeiner@proton.me>
2026-04-13 20:45:18 +0200

    Auto commit during rebuild

--- user/desktop/hyprland/scripts/cycle-audio.nix
+++ user/desktop/hyprland/scripts/cycle-audio.nix
@@ -11,20 +11,40 @@
         name = "fine-cycle-audio";
 
         text = ''
+  # Get sink names (internal IDs)
   mapfile -t sinks < <(pactl list short sinks | awk '{print $2}')
   current=$(pactl get-default-sink)
 
   for i in "''${!sinks[@]}"; do
     if [[ "''${sinks[$i]}" == "$current" ]]; then
       next_index=$(( (i + 1) % ''${#sinks[@]} ))
+      selected="''${sinks[$next_index]}"
 
-      pactl set-default-sink "''${sinks[$next_index]}"
-      
-      notify-send "''${sinks[$next_index]}"
+      # Switch default sink
+      pactl set-default-sink "$selected"
 
+      # Move existing audio streams
       for input in $(pactl list short sink-inputs | awk '{print $1}'); do 
-        pactl move-sink-input "$input" "''${sinks[$next_index]}"
+        pactl move-sink-input "$input" "$selected"
       done 
+
+      # Get human-readable description
+      description=$(pactl list sinks | awk -v sink="$selected" '
+        $1 == "Name:" {found = ($2 == sink)}
+        found && $1 == "Description:" {
+          $1=""; sub(/^ /, ""); print; exit
+        }
+      ')
+
+      # Clean up the name a bit
+      pretty=$(echo "$description" | \
+        sed -e 's/ Analog Stereo//' \
+            -e 's/ Digital Stereo (HDMI)//' \
+            -e 's/ Output//' \
+            -e 's/ +/ /g')
+
+      # Fallback if something goes wrong
+      notify-send "Audio: ''${pretty:-$selected}"
 
       break
     fi 
@@ -34,3 +54,26 @@
     })
   ];
 }
+
+
+
+#         text = ''
+#   mapfile -t sinks < <(pactl list short sinks | awk '{print $2}')
+#   current=$(pactl get-default-sink)
+#
+#   for i in "''${!sinks[@]}"; do
+#     if [[ "''${sinks[$i]}" == "$current" ]]; then
+#       next_index=$(( (i + 1) % ''${#sinks[@]} ))
+#
+#       pactl set-default-sink "''${sinks[$next_index]}"
+#       
+#       notify-send "''${sinks[$next_index]}"
+#
+#       for input in $(pactl list short sink-inputs | awk '{print $1}'); do 
+#         pactl move-sink-input "$input" "''${sinks[$next_index]}"
+#       done 
+#
+#       break
+#     fi 
+#   done
+# '';
