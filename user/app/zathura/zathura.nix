95121ebd6d62b7ad4a6d6f0d59cf135ddba21f89
Lorenz Feiner <lorenzfeiner@proton.me>
2026-04-25 12:29:20 +0200

    Auto commit during rebuild

--- user/app/zathura/zathura.nix
+++ user/app/zathura/zathura.nix
@@ -1,7 +1,13 @@
-{ ... }:
+{ config, ... }:
 
 {
   programs.zathura = {
     enable = true;
+
+    extraConfig = ''
+      set font "JetBrainsMono Nerd Font 13"
+
+      set statusbar-fg "#${config.lib.stylix.colors.base05}"
+    '';
   };
 }
