6289b6c1a8931ab75428794cdfc444b579db531d
Lorenz Feiner <lorenzfeiner@proton.me>
2026-04-25 11:36:26 +0200

    Auto commit during rebuild

--- user/desktop/hyprland/hyprland/group.nix
+++ user/desktop/hyprland/hyprland/group.nix
@@ -1,10 +1,13 @@
-{  ... }:
+{ config, ... }:
+    #@define-color workspaces-active       #${config.lib.stylix.colors.base0E};
 
 {
   wayland.windowManager.hyprland.settings.group = {
     groupbar = {
       enabled = true;
-      font_size = 12;
+      font_size = 16;
     };
+
+    col.border_active = "0x66${config.lib.stylix.colors.base0D}";
   };
 }
