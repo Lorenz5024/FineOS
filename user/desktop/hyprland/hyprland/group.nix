1e4b533e6210deccb3642d9f50555b2cf4e20c14
Lorenz Feiner <lorenzfeiner@proton.me>
2026-04-25 11:45:49 +0200

    Auto commit during rebuild

--- user/desktop/hyprland/hyprland/group.nix
+++ user/desktop/hyprland/hyprland/group.nix
@@ -6,8 +6,12 @@
     groupbar = {
       enabled = true;
       font_size = 16;
+
+      "col.active" = "0xff${config.lib.stylix.colors.base0D}";
+      "col.inactive" = "0xff${config.lib.stylix.colors.base02}";
+      "text_color" = "0xff${config.lib.stylix.colors.base05}";
     };
 
-    "col.border_active" = "0x66${config.lib.stylix.colors.base0D}";
+    "col.border_active" = "0xff${config.lib.stylix.colors.base0D}";
   };
 }
