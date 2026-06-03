4746fe33e284cf51316eb98a88eb727aeee0890c
Lorenz Feiner <lorenzfeiner@proton.me>
2026-06-03 23:05:13 +0200

    Auto commit during rebuild

--- user/app/ashell/ashell.nix
+++ user/app/ashell/ashell.nix
@@ -1,5 +1,10 @@
-{ pkgs, ... }:
+{ config, pkgs, userSettings, ... }:
 
+let 
+  configPath = "${userSettings.flakeDir}/user/app/ashell";
+in
 {
   home.packages = [ pkgs.ashell ];
+
+  xdg.configFile."ashell/config.toml".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/config.toml";
 }
