972615521f7092694babdba832b1425dea77eccb
Lorenz Feiner <lorenzfeiner@proton.me>
2026-06-04 22:59:17 +0200

    Auto commit during rebuild

--- user/themes/themes.nix
+++ user/themes/themes.nix
@@ -11,9 +11,5 @@ in
   xdg.configFile."hypr/theme.lua".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/hyprland/theme.lua";
 
   # waybar
-  # xdg.configFile."waybar/style.css".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/waybar/style.css";
-  # xdg.configFile."waybar/layout.jsonc".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/waybar/layout.jsonc";
-  # xdg.configFile."waybar/config.jsonc".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/waybar/config.jsonc";
-  # xdg.configFile."waybar/modules.jsonc".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/waybar/modules.jsonc";
   xdg.configFile."waybar/".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/waybar/";
 }
