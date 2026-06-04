875f953aeb3a3a8701262215ff2b4638a3a2ca47
Lorenz Feiner <lorenzfeiner@proton.me>
2026-06-04 22:55:37 +0200

    Auto commit during rebuild

--- user/themes/themes.nix
+++ user/themes/themes.nix
@@ -4,10 +4,16 @@ let
   configPath = "${userSettings.flakeDir}/user/themes/current";
 in
 {
+  # rofi
   xdg.configFile."rofi/themes/theme-launcher.rasi".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/rofi/theme-launcher.rasi";
+
+  # hyprland 
   xdg.configFile."hypr/theme.lua".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/hyprland/theme.lua";
-  xdg.configFile."waybar/style.css".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/waybar/style.css";
-  xdg.configFile."waybar/layout.jsonc".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/waybar/layout.jsonc";
-  xdg.configFile."waybar/config.jsonc".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/waybar/config.jsonc";
-  xdg.configFile."waybar/modules.jsonc".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/waybar/modules.jsonc";
+
+  # waybar
+  # xdg.configFile."waybar/style.css".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/waybar/style.css";
+  # xdg.configFile."waybar/layout.jsonc".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/waybar/layout.jsonc";
+  # xdg.configFile."waybar/config.jsonc".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/waybar/config.jsonc";
+  # xdg.configFile."waybar/modules.jsonc".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/waybar/modules.jsonc";
+  xdg.configFile."waybar/".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/waybar/";
 }
