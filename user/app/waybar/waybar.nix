a1aa529b776cec3e85b15be50c5735acc08c7ace
Lorenz Feiner <lorenzfeiner@proton.me>
2026-06-04 22:30:24 +0200

    Auto commit during rebuild

--- user/app/waybar/waybar.nix
+++ user/app/waybar/waybar.nix
@@ -6,19 +6,20 @@ in
 {
 
   xdg.configFile."waybar/style.css".source = config.lib.file.mkOutOfStoreSymlink "${themePath}/style.css";
+  xdg.configFile."waybar/layout.jsonc".source = config.lib.file.mkOutOfStoreSymlink "${themePath}/layout.jsonc";
 
-  imports = [ (./. + "/themes" + ( "/" + userSettings.waybarStyle) + ".nix") ];
+  # imports = [ (./. + "/themes" + ( "/" + userSettings.waybarStyle) + ".nix") ];
 
   programs.waybar = {
     enable = true;
 
     settings = {
       mainBar = {
-        include = "${userSettings.flakeDir}/user/app/waybar/modules";
-        layer = "top";
-        position = "top";
-        spacing = 0;
-	reload_style_on_change = true;
+        include = "${userSettings.flakeDir}/user/app/waybar/modules.jsonc";
+        # layer = "top";
+        # position = "top";
+        # spacing = 0;
+	# reload_style_on_change = true;
 
       };
 
