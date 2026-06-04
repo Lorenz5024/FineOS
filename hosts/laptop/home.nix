381f3b13c37aec7ab5057835f2886f0d997b8c16
Lorenz Feiner <lorenzfeiner@proton.me>
2026-06-04 09:26:57 +0200

    .

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -7,16 +7,11 @@
   ] ++ map (e: ./. + "/../../user/desktop/${e}/desktop.nix") hostSettings.desktops;
 
   # Set up monitors for hyprland
-  wayland.windowManager.hyprland.settings = {
-    monitor = [
-      # desktop setup
-      # "DP-4, 2560x1440@75, 0x0, 1"
-      # "DP-2, 2560x1440@60, 2560x0, 1" 
-      # "eDP-1, 2256x1504, 5120x0, 1.333333"
-
-      # just laptop
-      "eDP-1, 2256x1504, 0x0, 1.333333"
-      ", disable"
-    ];
-  };
+  xdg.configFile."hypr/hardware.lua".text = ''
+    hl.monitor({
+      output = "eDP-1",
+      mode = "2256x1504",
+      scale = 1.333333,
+    })
+  '';
 }
