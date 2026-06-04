f926ac328b758c79be10b50ae3d386503e8d79cc
Lorenz Feiner <lorenzfeiner@proton.me>
2026-06-04 22:52:58 +0200

    Auto commit during rebuild

--- user/themes/themes.nix
+++ user/themes/themes.nix
@@ -6,4 +6,8 @@ in
 {
   xdg.configFile."rofi/themes/theme-launcher.rasi".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/rofi/theme-launcher.rasi";
   xdg.configFile."hypr/theme.lua".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/hyprland/theme.lua";
+  xdg.configFile."waybar/style.css".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/waybar/style.css";
+  xdg.configFile."waybar/layout.jsonc".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/waybar/layout.jsonc";
+  xdg.configFile."waybar/config.jsonc".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/waybar/config.jsonc";
+  xdg.configFile."waybar/modules.jsonc".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/waybar/modules.jsonc";
 }
