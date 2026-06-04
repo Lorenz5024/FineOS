bcd62c3dda892c73cec9a187a858b85244edf415
Lorenz Feiner <lorenzfeiner@proton.me>
2026-06-04 22:49:22 +0200

    Auto commit during rebuild

--- user/app/waybar/waybar.nix
+++ user/app/waybar/waybar.nix
@@ -1,32 +1,10 @@
-{ config, userSettings, ... }:
+{ ... }:
 
-let 
-  themePath = "${userSettings.flakeDir}/user/themes/current/waybar";
-in
 {
 
-  xdg.configFile."waybar/style.css".source = config.lib.file.mkOutOfStoreSymlink "${themePath}/style.css";
-  xdg.configFile."waybar/layout.jsonc".source = config.lib.file.mkOutOfStoreSymlink "${themePath}/layout.jsonc";
-  xdg.configFile."waybar/config.jsonc".source = config.lib.file.mkOutOfStoreSymlink "${themePath}/config.jsonc";
-  xdg.configFile."waybar/modules.jsonc".source = config.lib.file.mkOutOfStoreSymlink "${themePath}/modules.jsonc";
-
-  # imports = [ (./. + "/themes" + ( "/" + userSettings.waybarStyle) + ".nix") ];
 
   programs.waybar = {
     enable = true;
-
-    # settings = {
-      # mainBar = {
-        # include = "${userSettings.flakeDir}/user/app/waybar/modules.jsonc";
-        # layer = "top";
-        # position = "top";
-        # spacing = 0;
-	# reload_style_on_change = true;
-
-      # };
-
-    # };
-
   };
 
 }
