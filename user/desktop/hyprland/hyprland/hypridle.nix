31785feacfa6ba82558521db65bff6ea6c7cb055
Lorenz Feiner <lorenzfeiner@proton.me>
2026-06-09 19:10:16 +0200

    Auto commit during rebuild

--- user/desktop/hyprland/hyprland/hypridle.nix
+++ user/desktop/hyprland/hyprland/hypridle.nix
@@ -9,7 +9,7 @@
       general = {
           lock_cmd = "pidof hyprlock || hyprlock";        # avoid starting multiple hyprlock instances.
           before_sleep_cmd = "hyprlock";	          # lock before suspend.
-          after_sleep_cmd = "hyprctl dispatch dpms on";   # to avoid having to press a key twice to turn on the display.
+          after_sleep_cmd = "hyprctl dispatch 'hl.dsp.dpms({ action = \"enable\" })' && brightnessctl -r";   # to avoid having to press a key twice to turn on the display.
       };
 
       listener = [
